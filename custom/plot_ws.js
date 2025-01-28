38462a7449322b16c99cc6e7b4382c604d6d1077
Radovan Ristic <ristic@IBR.local>
2025-01-28 16:16:56 +0100

    horizontal lines

--- custom/plot_ws.js
+++ custom/plot_ws.js
@@ -16,32 +16,15 @@ punkte_pegel.on('click', function(e) {
 var lastXValue_nominal = null;
 var lastXValue = null;
 var startXValue = null;
+
 function updatePlotWS(markerId) {
     console.log('Updating ws_plot with marker ID:', markerId);
     currentMarkerIdWS = markerId;
 
     // Convert timestamp strings to Date objects for x-axis
-    // calculations and measured data during preprocessing  are converted to UTC-time which is also displayed on the x axis but it acts like local time zone
     const xValues_messung = Object.keys(ws_messung[markerId]).map(dateStr => new Date(dateStr));
-    
-    /*
-    // it is that complicated to make it time zone aware
-    const xValues_messung = Object.keys(ws_messung[markerId]).map(dateStr => {
-    const date = new Date(dateStr);
-    const utcDate = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()));
-    const options = { timeZone: 'Europe/Berlin', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-    const formattedDate = new Intl.DateTimeFormat('de-DE', options).format(utcDate);
-    const [day, month, year, hour, minute, second] = formattedDate.match(/\d+/g);
-    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`);
-    });
-    */
-
-
-    //const xValues_messung = Object.keys(ws_messung[markerId]);
     const yValues_messung = Object.values(ws_messung[markerId]);
-    //console.log(xValues_messung)
-    // Calculate the range: last x value and the 48th value before it 
-    //startXValue = xValues_messung[Math.max(xValues_messung.length - 49, 0)];
+
     lastXValue_nominal = xValues_messung[xValues_messung.length - 1]; 
     startXValue = new Date(lastXValue_nominal.getTime() - 18 * 60 * 60 * 1000); // 24 hours before lastX
     lastXValue = new Date(lastXValue_nominal.getTime() + 12 * 60 * 60 * 1000); // 6 or 12 hours after lastX
@@ -55,8 +38,7 @@ function updatePlotWS(markerId) {
         type: 'scatter',
         mode: 'lines',
         name: 'Messung',
-        line: { color: '#6d6af2' },
-        //hovertemplate: '%{x|%Y-%m-%d %H:%M}<br>Wasserstand: %{y}'
+        line: { color: '#6d6af2' }
     };
 
     const trace2 = {
@@ -65,13 +47,57 @@ function updatePlotWS(markerId) {
         type: 'scatter',
         mode: 'lines',
         name: 'Prognose',
-        line: { color: '#e84f7a' },
-        //hovertemplate: '%{x|%Y-%m-%d %H:%M}<br>Wasserstand: %{y}'
+        line: { color: '#e84f7a' }
     };
+
+    // Calculate the maximum y-value and add 10%
+    const maxYValue = Math.max(...yValues_messung);
+    const maxYValuePlus15Percent = maxYValue * 1.15;
+
+    // Check for HW values and add horizontal lines if markerId matches
+    var schwellenwerte = {
+        "M-001": { "HW100": 352, "HW50": 335, "HW20": 312, "HW10": 295, "HW2": 248, "MW": 64, "MNW": 23 },
+        "M-002": { "HW100": 356, "HW50": 332, "HW20": 299, "HW10": 272, "HW2": 207, "MW": 60, "MNW": 34 },
+        "M-003": { "HW100": 293, "HW50": 260, "HW20": 210, "HW10": 169, "HW2": 102, "MW": 32, "MNW": 4 },
+        "M-004": { "HW100": 363, "HW50": 340, "HW20": 305, "HW10": 279, "HW2": 221, "MW": 74, "MNW": 49 },
+        "M-005": {},
+        "M-006": { "HW100": 235, "HW50": 217, "HW20": 193, "HW10": 175, "HW2": 134, "MW": 38, "MNW": 22 },
+        "M-007": {},
+        "M-008": {}
+    };
+
+    var hwKeys = {
+        "HW2": "rgb(243,240,60)",
+        "HW10": "rgb(248,174,100)",
+        "HW20": "rgb(222,10,20)",
+        "HW50": "rgb(138,45,135)",
+        "HW100": "rgb(91,16,91)"
+    };
+
+    var horizontalLines = [];
+    if (schwellenwerte.hasOwnProperty(markerId)) {
+        for (var key in hwKeys) {
+            if (schwellenwerte[markerId].hasOwnProperty(key)) {
+                horizontalLines.push({
+                    type: 'line',
+                    x0: xValues_messung[0],
+                    x1: lastXValue,
+                    y0: schwellenwerte[markerId][key],
+                    y1: schwellenwerte[markerId][key],
+                    line: {
+                        color: hwKeys[key],
+                        width: 3,
+                        dash: 'dot'  // Change this to 'solid', 'dash', 'dot', 'dashdot', 'longdash', or 'longdashdot'
+                    },
+                    layer: 'below'
+                });
+            }
+        }
+    }
+
     const layout = {
         title: {
             text: `Pegel: ${currentPegel}`,
-            //text: `Pegel: ${markerId}`,
             font: {
                 family: 'Arial, sans-serif',
                 size: 15
@@ -81,22 +107,22 @@ function updatePlotWS(markerId) {
             xanchor: 'center',
             y: 0.98
         },
-        xaxis: { rangemode: 'normal', 
-            //title: 'Time', 
+        xaxis: { 
+            rangemode: 'normal', 
             tickformat: '%d.%m.%Y %H:%M',
             tickfont: { size: 8 }, 
-            range: [startXValue, lastXValue], // Set x-axis range here
+            range: [startXValue, lastXValue]
         },
         yaxis: { 
             title: {
-                text:'Wasserstand [cm]', 
-                font: {size:11},
-                },
+                text: 'Wasserstand [cm]', 
+                font: { size: 11 }
+            },
             zeroline: true, 
             rangemode: 'tozero', 
-            autorange: true, 
+            range: [0, maxYValuePlus15Percent], // Set y-axis range here
             tickformat: '.0f', 
-            tickfont: { size: 10 } ,
+            tickfont: { size: 10 }
         },
         legend: {
             orientation: 'v',
@@ -107,7 +133,6 @@ function updatePlotWS(markerId) {
             font: { size: 9 },
             bgcolor: 'rgba(255,255,255,0.5)'
         },
-        
         margin: {
             l: 40,  // left margin
             r: 10,  // right margin
@@ -115,7 +140,7 @@ function updatePlotWS(markerId) {
             b: 25   // bottom margin
         },
         hovermode: 'x',
-        
+        shapes: horizontalLines
     };
     
     Plotly.newPlot('plotWS', [trace1, trace2], layout, { displayModeBar: false });
@@ -123,19 +148,15 @@ function updatePlotWS(markerId) {
 
     toggleZoomButton.onclick = () => { 
         Plotly.relayout('plotWS', { 
-        'xaxis.range': [startXValue, lastXValue],
-        'yaxis.rangemode': 'tozero',
-        'yaxis.autorange': true,
+            'xaxis.range': [startXValue, lastXValue],
+            'yaxis.range': [0, maxYValuePlus15Percent]
         }); 
     }; 
     
     toggleZoomButton2.onclick = () => { 
         Plotly.relayout('plotWS', { 
-        'xaxis.range': [xValues_messung[0], lastXValue],
-        'yaxis.rangemode': 'tozero',
-        'yaxis.autorange': true,
+            'xaxis.range': [xValues_messung[0], lastXValue],
+            'yaxis.range': [0, maxYValuePlus15Percent]
         }); 
     }; 
-
 }
-
