7343e504338f1a632e72fcc3a0cba626208823f7
Radovan Ristic <ristic@IBR>
2025-05-14 08:42:56 +0200

    080

--- data/PegelaktuelleWasserstnde_44.js
+++ data/PegelaktuelleWasserstnde_44.js
@@ -136,7 +136,7 @@ var json_PegelaktuelleWasserstnde_44 = {
                 "Gew\u00e4sser": "Salm",
                 "EZG": 20.4,
                 "Intern_Bez": "M-007",
-                "Farbe": "-1",
+                "Farbe": "1",
                 "Link": "https://geodaten-wasser.rlp-umwelt.de/wasserstand/2674028400/stammdaten",
                 "Link_kurz": "<a href=\"https://geodaten-wasser.rlp-umwelt.de/wasserstand/2674028400/stammdaten\" target=\"_blank\" rel=\"noopenernoreferrer\">Link</a>"
             },
