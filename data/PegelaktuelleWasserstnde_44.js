ad843b40938781a601fbc40409fa5bb90b642033
Radovan Ristic <ristic@IBR.local>
2025-12-05 09:37:53 +0100

    179

--- data/PegelaktuelleWasserstnde_44.js
+++ data/PegelaktuelleWasserstnde_44.js
@@ -96,7 +96,7 @@ var json_PegelaktuelleWasserstnde_44 = {
                 "Gew\u00e4sser": "Lieser",
                 "EZG": 274.1,
                 "Intern_Bez": "M-002",
-                "Farbe": "4",
+                "Farbe": "3",
                 "Link": "https://geodaten-wasser.rlp-umwelt.de/wasserstand/2678045000/stammdaten",
                 "Link_kurz": "<a href=\"https://geodaten-wasser.rlp-umwelt.de/wasserstand/2678045000/stammdaten\" target=\"_blank\" rel=\"noopenernoreferrer\">Link</a>"
             },
